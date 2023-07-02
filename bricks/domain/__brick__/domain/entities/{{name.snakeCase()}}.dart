c94026db0cba38aa86f61bdfb317b6ea01d8a49c
MS Arifin <ms.arifin29@gmail.com>
2023-07-02 17:32:44 +0800

    feat : add bricks domain

---
+++ bricks/domain/__brick__/domain/entities/{{name.snakeCase()}}.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class {{name.pascalCase()}} extends Equatable {
+const {{name.pascalCase()}}();
+
+@override
+List<Object?> get props => [];
+}
---
+++ bricks/domain/__brick__/domain/usecases/get_{{name.snakeCase()}}.dart
@@ -0,0 +1,1 @@
+class Get{{name.pascalCase()}}{}
